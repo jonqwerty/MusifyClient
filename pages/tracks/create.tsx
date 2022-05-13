30adea7ff85dafca0942deb16ce37b2b21160cef
Serhii B <s_blyshchak@ukr.net>
2022-05-13 22:30:56 +0300

    Implement upload picture and audio logic

--- pages/tracks/create.tsx
+++ pages/tracks/create.tsx
@@ -1,10 +1,13 @@
 import { Button, Grid, TextField } from "@mui/material"
 import React, { useState } from "react"
+import FileUpload from "../../components/FileUpload"
 import StepWrapper from "../../components/StepWrapper"
 import MainLayout from "../../layouts/MainLayout"
 
 const Create = () => {
   const [activeStep, setActiveStep] = useState(0)
+  const [picture, setPicture] = useState(null)
+  const [audio, setAudio] = useState(null)
 
   const next = () => {
     if (activeStep !== 2) {
@@ -24,8 +27,12 @@ const Create = () => {
             <TextField style={{ marginTop: 10 }} label={"Слова до треку"} multiline rows={3} />
           </Grid>
         )}
-        {activeStep === 1 && <h1>step 2</h1>}
-        {activeStep === 2 && <h1>step 3</h1>}
+        {activeStep === 1 && <FileUpload setFile={setPicture} accept='image/*' >
+<Button>Завантажити зображення</Button>
+          </FileUpload>}
+        {activeStep === 2 && <FileUpload setFile={setAudio} accept='audio/*' >
+<Button>Завантажити аудіо</Button>
+          </FileUpload>}}
       </StepWrapper>
       <Grid container justifyContent="space-between">
         <Button disabled={activeStep === 0} onClick={back}>
